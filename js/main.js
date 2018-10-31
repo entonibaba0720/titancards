aa1db24b353853793b5516cc853646d35dbebed6
entonibaba0720 <entonibaba0720@gmail.com>
2018-10-31 10:34:45 +0100

    m

--- js/main.js
+++ js/main.js
@@ -1,20 +1,17 @@
-$(document).ready(function(){
+$(document).ready(function() {
     var swiper = new Swiper({
         el: '.swiper-container',
         initialSlide: 3,
         loop: true,
         slideToClickedSlide: true,
-        grabCursor: true,
+
+
         pagination: {
-          el: '.swiper-pagination',
+            el: '.swiper-pagination',
+
         },
-        renderBullet: function (index, className) {
-          return '<span class="' + className + '">' + (index + 1) + '</span>';
+        renderBullet: function(index, className) {
+            return '<span class="' + className + '">' + (index + 1) + '</span>';
         }
     });
 });
-
-
-
-
-

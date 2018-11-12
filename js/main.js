f3e9716d5cf64f7a72755effaddc99ef0b54c93e
entonibaba0720 <entonibaba0720@gmail.com>
2018-11-12 20:56:38 +0100

    r

--- js/main.js
+++ js/main.js
@@ -1,20 +1,37 @@
 $(document).ready(function() {
-    var swiper = new Swiper({
-        el: '.swiper-container',
+
+    // Section 1 swiper with pagination
+    var swiper1 = new Swiper({
+        el: '.swiper1',
         initialSlide: 3,
         loop: true,
         slideToClickedSlide: true,
-
-
         pagination: {
-            el: '.swiper-pagination',
-
+            el: '.swiper-pagination1',
+            paginationClickable: true,
         },
+    });
 
+    // Section 2 swiper with pagination
+    var swiper2 = new Swiper({
+        el: '.swiper2',
+        initialSlide: 0,
+        loop: true,
+        slidesPerView: 3,
+        preventClicksPropagation: false,
+        slideToClickedSlide: true,
+        pagination: {
+            el: '.swiper-pagination2',
+            paginationClickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
     });
 
 
-
+    // Menu icon change and show the menu
     $('.menu_icon').click(function() {
         $('#menu').animate({
             right: '0'
@@ -23,6 +40,7 @@ $(document).ready(function() {
         $('.menu_close').show();
     });
 
+    // X close the menu and change the menu icon
     $('.menu_close').click(function() {
         $('#menu').animate({
             right: '-100%'
@@ -31,21 +49,19 @@ $(document).ready(function() {
         $('.menu_close').hide();
     });
 
-    $(function() {
-
-        $(document).on('scroll', function() {
-
-            if ($(window).scrollTop() > 100) {
-                $('.logo_header').css('visibility', 'hidden');
-                $('.scroll-up').addClass('show');
-            } else {
-                $('.logo_header').css('visibility', 'visible');
-                $('.scroll-up').removeClass('show');
-            }
-        });
-
-
+    // Show arrows and hide logo when scrolling
+    $(document).on('scroll', function() {
+        if ($(window).scrollTop() > 100 && $(window).scrollTop() < $(window).height()) {
+            $('.logo_header').css('visibility', 'hidden');
+            $('.scroll-up').addClass('show');
+        } else {
+            $('.logo_header').css('visibility', 'visible');
+            $('.scroll-up').removeClass('show');
+        }
     });
+
+
+
 
 
 });

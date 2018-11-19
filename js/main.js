3094b9cc4deb7112c2dde439b9b7529b636daa6b
entonibaba0720 <entonibaba0720@gmail.com>
2018-11-19 21:20:30 +0100

    bbb

--- js/main.js
+++ js/main.js
@@ -80,6 +80,17 @@ $(document).ready(function() {
         }
     });
 
+    // Smooth scrolling
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+
 
 
 
